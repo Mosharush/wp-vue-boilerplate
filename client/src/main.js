5197569891a6b6ec978985bb4653c3f6dcbca8b4
Moshe Harush <upload770@gmail.com>
2020-08-04 19:58:36 +0300

    Global url setup via @wordpress/api-fetch middleware

--- client/src/main.js
+++ client/src/main.js
@@ -2,10 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import apiFetch from "@wordpress/api-fetch";
 
 Vue.config.productionTip = false;
 
+const apiRootURL = "https://localhost/wp-json/";
+apiFetch.use(apiFetch.createRootURLMiddleware(apiRootURL));
+
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
