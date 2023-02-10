d16b7f7959543aa6bb8c47049ff4883f276c5f8b
Moshe Harush <moshe@haru.sh>
2023-02-10 09:20:00 +0200

    Composition Api + use WpError Component

--- apps/front/src/views/HomeView.vue
+++ apps/front/src/views/HomeView.vue
@@ -1,26 +1,44 @@
 <template>
   <div class="home">
-    <ul>
+    <ul v-if="!error">
       <li v-for="post in posts" :key="post.id">
         <a :href="`post/${post.id}`">{{ post.title.rendered }}</a>
       </li>
     </ul>
+    <WpError v-else :error="error" />
   </div>
 </template>
 
 <script>
 import apiFetch from "@wordpress/api-fetch";
+import { ref } from "vue";
+import WpError from "../components/WpError.vue";
 
 export default {
-  name: "Home",
-  components: {},
-  data() {
-    return {
-      posts: [],
-    };
+  name: "HomeView",
+  components: {
+    WpError,
   },
-  async beforeMount() {
-    this.posts = await apiFetch({ path: "/wp-json/wp/v2/posts" });
+  setup() {
+    const posts = ref([]);
+    const error = ref(null);
+
+    async function fetchPosts() {
+      try {
+        posts.value = await apiFetch({ path: "/wp-json/wp/v2/posts" });
+      } catch (e) {
+        error.value = {
+          link: `${location.protocol}//${location.hostname}:8000`,
+          ...e,
+        };
+      }
+    }
+    fetchPosts();
+
+    return {
+      posts,
+      error,
+    };
   },
 };
 </script>
