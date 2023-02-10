39e36ab7ecbdef593fa46cc84afdb68e17613478
Moshe Harush <moshe@haru.sh>
2023-02-10 09:19:54 +0200

    WpError Component: to display apiFetch errors

---
+++ apps/front/src/components/WpError.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wp-error">
+    <h1>Error: {{ error.message }}</h1>
+    <hr/>
+    Try to check if the REST API is enabled in WordPress and all setup is correct.
+    <br/>
+    <a v-if="error.link" target="_blank" :href="error.link">
+      Go to Wordpress site
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WpError",
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
